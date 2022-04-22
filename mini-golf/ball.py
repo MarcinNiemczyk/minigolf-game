94f1fb30ef1d7490eb4bb4122026c2c34755618d
martindustry <marcin.exn@gmail.com>
2022-04-22 12:27:14 +0200

    feat: ball movement physics

--- mini-golf/ball.py
+++ mini-golf/ball.py
@@ -1,33 +1,18 @@
 import pygame
+import commons
 
 
 class Ball:
-	def __init__(self, main):
-		self.screen = main.screen
-		self.settings = main.settings
-		self.screen_rect = self.screen.get_rect()
+	def __init__(self):
+		self.image = pygame.image.load("images/ball.png").convert_alpha()
+		self.rect = self.image.get_rect()
+		self.rect.center = commons.initial_ball_pos
+		self.velocity_x = 0.0
+		self.velocity_y = 0.0
 
-		self.image = pygame.image.load('images/ball.png').convert_alpha()
-		self.image = pygame.transform.scale(self.image,
-		                                    (self.settings.ball_size,
-		                                     self.settings.ball_size))
-		self.pos = self.image.get_rect()
-		self.pos.center = self.screen_rect.center
-		self.y = float(self.pos.y)
-
-	def update(self, end_x, end_y):
-		end_x = end_x - self.settings.ball_size // 2
-		end_y = end_y - self.settings.ball_size // 2
-		if self.pos.x > end_x:
-			self.pos.x -= 1
-		elif self.pos.x < end_x:
-			self.pos.x += 1
-
-		if self.pos.y > end_y:
-			self.pos.y -= 1
-		elif self.pos.y < end_y:
-			self.pos.y += 1
-
+		# Float values of a ball position vector are needed to handle movement.
+		self.x = float(self.rect.centerx)
+		self.y = float(self.rect.centery)
 
 	def draw(self):
-		self.screen.blit(self.image, self.pos)
+		commons.screen.blit(self.image, self.rect)
---
+++ mini-golf/commons.py
@@ -0,0 +1,12 @@
+"""A file that store commonly used variables in one place."""
+
+screen_width = 800
+screen_height = 600
+fps = 60
+delta_time = 1 / fps
+friction = 0.981
+
+bg_color = (90, 170, 56)
+screen = None
+
+initial_ball_pos = (screen_width/2, screen_height/2)
